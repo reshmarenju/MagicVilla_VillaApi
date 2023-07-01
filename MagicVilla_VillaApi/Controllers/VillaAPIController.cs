29aa2087ed7c6bf41c4f7e359bd1716a71432c3b
Reshma Raj <reshma.raj@speehive.com>
2023-07-01 23:54:39 +0530

    First end point

---
+++ MagicVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id=1,Name="pool view"}
+                ,new Villa() {Id=2,Name="beach view"}
+            };
+        }
+    }
+}
---
+++ MagicVilla_VillaApi/Model/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaApi.Model
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
